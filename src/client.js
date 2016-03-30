cfe5e70c3b1fb35330a903d29b7618bdc387d24a
Jason <jasonlmcaffee@gmail.com>
2016-03-30 13:48:39 -0600

    IMP-389- initial commit

---
+++ src/client.js
@@ -0,0 +1,47 @@
+import request from 'request';
+
+/**
+ *
+ * @param baseUrl
+ * @constructor
+ */
+function ImpChannelManagerClient(baseUrl){
+  this.baseUrl = baseUrl;
+}
+
+ImpChannelManagerClient.prototype = {
+  /**
+   *
+   * @param pmcId
+   * @param listingType
+   * @returns {Promise}
+   */
+  getChannelsByPropertyAndType: function(pmcId, listingType){
+    let baseUrl = this.baseUrl;
+    return new Promise((resolve, reject) => {
+      request({
+        method: 'GET',
+        url: baseUrl + `/${pmcId}/${listingType}`
+      }, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        try {
+          body = JSON.parse(body);
+        } catch (err) {
+          reject(err);
+        }
+        resolve(body);
+      });
+    });
+  }
+};
+
+/**
+ *
+ * @param baseUrl
+ * @returns {ImpChannelManagerClient}
+ */
+export let createChannelManagerClientClient = (baseUrl)=>{
+  return new ImpChannelManagerClient(baseUrl);
+};
